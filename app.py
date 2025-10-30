35005ba88b077635f11be70d5c5ea61d7da4e2c6
Monu Das <jyotsnadas090@gmail.com>
2025-10-30 12:31:51 +0530

    Initial commit - Smart Stock Chooser

--- app.py
+++ app.py
@@ -76,6 +76,7 @@ def home():
     return render_template_string(html, data=data)
 
 if __name__ == "__main__":
-    print("🚀 Starting Flask server... Open http://127.0.0.1:5000 in your browser.")
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
